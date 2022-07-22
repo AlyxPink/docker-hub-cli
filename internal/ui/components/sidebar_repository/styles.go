536f56aca39d0af7a1c39727e1eaad0144641db2
Victor Bersy <victor.bersy@hey.com>
2022-07-22 03:47:30 +0200

    Rework sidebar rendering

--- internal/ui/components/sidebar_repository/styles.go
+++ internal/ui/components/sidebar_repository/styles.go
@@ -5,39 +5,27 @@ import (
 )
 
 var (
-	labelDockerOfficial    = lipgloss.Color("#2E7F74")
-	labelVerifiedPublisher = lipgloss.Color("#086DD7")
-	labelOpenSourceProgram = lipgloss.Color("#7D2EFF")
-	archTagFg              = lipgloss.AdaptiveColor{Light: "#22223b", Dark: "#EDEDE9"}
-	archTagBg              = lipgloss.AdaptiveColor{Light: "#bde0fe", Dark: "#0077b6"}
-	dockerImageTagFg       = lipgloss.Color("#22223b")
-	dockerImageTagBg       = lipgloss.Color("#EDEDE9")
-	dockerPullCmdBoxBg     = lipgloss.AdaptiveColor{Light: "#00f5d4", Dark: "#8ac926"}
-	dockerPullCmdBoxFg     = lipgloss.Color("#22223b")
+	archTagFg          = lipgloss.AdaptiveColor{Light: "#22223b", Dark: "#EDEDE9"}
+	archTagBg          = lipgloss.AdaptiveColor{Light: "#bde0fe", Dark: "#0077b6"}
+	dockerPullCmdBoxBg = lipgloss.AdaptiveColor{Light: "#00f5d4", Dark: "#8ac926"}
+	dockerPullCmdBoxFg = lipgloss.Color("#22223b")
 
-	label = lipgloss.NewStyle().
-		Padding(0, 1).
-		Bold(false)
+	title      = lipgloss.NewStyle().Bold(true).MarginBottom(1)
+	titleRepo  = title.Copy().Margin(0, 1, 1, 0)
+	labelGlyph = title.Copy().Margin(0, 1, 1, 0)
 
-	labelTitle = label.Copy().
-			Underline(true).
-			Bold(true).
-			Margin(0, 1, 1, 0)
+	description = lipgloss.NewStyle().Margin(1, 0, 2, 0)
 
-	archTitle = labelTitle
-	archTag   = label.Copy().
+	archsTitle = title.Copy().Underline(true)
+	archLabel  = lipgloss.NewStyle().
+			MarginTop(1).
+			MarginRight(1).
+			Padding(0, 1).
 			Background(archTagBg).
-			Foreground(archTagFg).
-			Margin(0, 1)
+			Foreground(archTagFg)
 
-	dockerImageTitle = labelTitle.Copy()
-	dockerImageTag   = label.Copy().
-				Background(dockerImageTagBg).
-				Foreground(dockerImageTagFg).
-				Margin(0, 1)
-
-	dockerPullCmdTitle = labelTitle.Copy()
-	dockerPullCmdBox   = label.Copy().
+	dockerPullCmdTitle = title.Copy().Underline(true).MarginTop(2)
+	dockerPullCmdBox   = lipgloss.NewStyle().
 				Background(dockerPullCmdBoxBg).
 				Foreground(dockerPullCmdBoxFg).
 				Padding(2).
