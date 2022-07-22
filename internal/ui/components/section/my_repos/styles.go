68b203f07ad30cae3df1da5fbe1c0ab54c95cabf
Alyx <github@alyx.pink>
2022-07-22 22:45:44 +0200

    Move section to their own directories

---
+++ internal/ui/components/section/my_repos/styles.go
@@ -0,0 +1,32 @@
+package my_repos
+
+import (
+	"github.com/VictorBersy/docker-hub-cli/internal/ui/components/section"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	nameWidth              = 25
+	organizationsnameWidth = 20
+	LastUpdateCellWidth    = lipgloss.Width(" Last Update ")
+	labelsWidth            = 12
+	statsWidth             = 8
+
+	statsDownloads = lipgloss.AdaptiveColor{Light: "#00BBF9", Dark: "#00BBF9"}
+	statsStars     = lipgloss.AdaptiveColor{Light: "#FFB703", Dark: "#FFB703"}
+
+	columnTitleStatsDownloads = section.ColumnTitle.Copy().Foreground(statsDownloads)
+	columnTitleStatsStars     = section.ColumnTitle.Copy().Foreground(statsStars)
+
+	ContainerPadding = 1
+
+	containerStyle = lipgloss.NewStyle().
+			Padding(0, ContainerPadding)
+
+	spinnerStyle = lipgloss.NewStyle().Padding(0, 1)
+
+	emptyStateStyle = lipgloss.NewStyle().
+			Faint(true).
+			PaddingLeft(1).
+			MarginBottom(1)
+)
