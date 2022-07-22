71255311f0b40277197b736454d1e863a5f840d9
Alyx <github@alyx.pink>
2022-07-22 22:45:24 +0200

    Add debug option

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/VictorBersy/docker-hub-cli/internal/ui"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
 	p := tea.NewProgram(
 		ui.NewModel(),
 		tea.WithAltScreen(),
